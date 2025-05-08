3a23e09cc3f4e010277fa2371a0a64e14a9a4382
SirTee <ojotaye33@gmail.com>
2025-05-08 16:03:06 +0100

    Updated API URL and fixed contact form

--- drum-app/src/Pages/Contacts.jsx
+++ drum-app/src/Pages/Contacts.jsx
@@ -35,7 +35,8 @@ const Contacts = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/send-email', formData);
+      await axios.post(`${process.env.REACT_APP_API_URL}/api/contact`, formData); /*('https://iya-ilu-backend.onrender.com') await  await axios.post('http://localhost:5000/send-email', formData);
+      */
       toast.success('Message sent successfully!');
       setFormData({ name: '', email: '', message: '' });
     } catch (error) {
