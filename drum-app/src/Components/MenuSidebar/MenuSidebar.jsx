fac67ef9b25efcf810f88a543e97f1fa171a5000
SirTee <ojotaye33@gmail.com>
2025-10-20 21:55:18 +0100

    Updated drum-app

--- drum-app/src/Components/MenuSidebar/MenuSidebar.jsx
+++ drum-app/src/Components/MenuSidebar/MenuSidebar.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './MenuSidebar.css'; // (new CSS for left sidebar)
+import { FaUser, FaBookOpen, FaQuestionCircle, FaEnvelope } from 'react-icons/fa';
+import './MenuSidebar.css';
 
-
-const MenuSidebar = ({ handleCloseMenu, isOpen  }) => {
+const MenuSidebar = ({ handleCloseMenu, isOpen }) => {
   return (
     <div className={`menu-sidebar ${isOpen ? 'open' : ''}`}>
       <button onClick={handleCloseMenu} className="close-btn">❌</button>
 
       <ul className="menu-list">
         <li>
-          <Link to="/about" onClick={handleCloseMenu}>About</Link>
+          <Link to="/about" onClick={handleCloseMenu}>
+            <FaUser className="menu-icon about-icon" /> About
+          </Link>
         </li>
         <li>
-          <Link to="/content" onClick={handleCloseMenu}>Content</Link>
+          <Link to="/content" onClick={handleCloseMenu}>
+            <FaBookOpen className="menu-icon content-icon" /> Content
+          </Link>
         </li>
         <li>
-          <Link to="/askme" onClick={handleCloseMenu}>AskMe</Link>
+          <Link to="/askme" onClick={handleCloseMenu}>
+            <FaQuestionCircle className="menu-icon ask-icon" /> AskMe
+          </Link>
         </li>
         <li>
-          <Link to="/contacts" onClick={handleCloseMenu}>Contact</Link>
+          <Link to="/contacts" onClick={handleCloseMenu}>
+            <FaEnvelope className="menu-icon contact-icon" /> Contact
+          </Link>
         </li>
       </ul>
     </div>
